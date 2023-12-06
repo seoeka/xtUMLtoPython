b20a4573ca80dfc18dcd01d305fdd1d2601e5cf3
eka <21106050023@student.uin-suka.ac.id>
2023-12-06 10:53:19 +0700

    add newtonsoft and copy buttons

--- pppl-uml-python/Form1.Designer.cs
+++ pppl-uml-python/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
+            this.textGeneratePython = new System.Windows.Forms.Label();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnUpload = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bt_copyPy = new System.Windows.Forms.Button();
+            this.bt_copyJSON = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,34 +52,37 @@
             // panel1
             // 
             this.panel1.AutoScroll = true;
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textGeneratePython);
             this.panel1.Location = new System.Drawing.Point(416, 51);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(358, 352);
             this.panel1.TabIndex = 1;
             // 
-            // label2
+            // textGeneratePython
             // 
-            this.label2.Location = new System.Drawing.Point(3, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(352, 324);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "label2";
+            this.textGeneratePython.AutoSize = true;
+            this.textGeneratePython.Location = new System.Drawing.Point(3, 9);
+            this.textGeneratePython.Name = "textGeneratePython";
+            this.textGeneratePython.Size = new System.Drawing.Size(35, 13);
+            this.textGeneratePython.TabIndex = 0;
+            this.textGeneratePython.Text = "label2";
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(26, 403);
+            this.btnGenerate.Location = new System.Drawing.Point(218, 403);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(202, 23);
+            this.btnGenerate.Size = new System.Drawing.Size(178, 23);
             this.btnGenerate.TabIndex = 2;
             this.btnGenerate.Text = "Generate to Python";
             this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(416, 409);
+            this.btnClear.Location = new System.Drawing.Point(608, 409);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(202, 23);
+            this.btnClear.Size = new System.Drawing.Size(166, 23);
             this.btnClear.TabIndex = 4;
             this.btnClear.Text = "Clear All Data";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -101,11 +106,33 @@
             this.textBox1.Size = new System.Drawing.Size(370, 346);
             this.textBox1.TabIndex = 3;
             // 
+            // bt_copyPy
+            // 
+            this.bt_copyPy.Location = new System.Drawing.Point(416, 409);
+            this.bt_copyPy.Name = "bt_copyPy";
+            this.bt_copyPy.Size = new System.Drawing.Size(186, 23);
+            this.bt_copyPy.TabIndex = 6;
+            this.bt_copyPy.Text = "Copy Code";
+            this.bt_copyPy.UseVisualStyleBackColor = true;
+            this.bt_copyPy.Click += new System.EventHandler(this.bt_copyPy_Click);
+            // 
+            // bt_copyJSON
+            // 
+            this.bt_copyJSON.Location = new System.Drawing.Point(26, 403);
+            this.bt_copyJSON.Name = "bt_copyJSON";
+            this.bt_copyJSON.Size = new System.Drawing.Size(186, 23);
+            this.bt_copyJSON.TabIndex = 7;
+            this.bt_copyJSON.Text = "Copy JSON";
+            this.bt_copyJSON.UseVisualStyleBackColor = true;
+            this.bt_copyJSON.Click += new System.EventHandler(this.bt_copyJSON_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_copyJSON);
+            this.Controls.Add(this.bt_copyPy);
             this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.textBox1);
@@ -113,8 +140,10 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UML to Python";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,11 +153,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label textGeneratePython;
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnUpload;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bt_copyPy;
+        private System.Windows.Forms.Button bt_copyJSON;
     }
 }
 
--- pppl-uml-python/Form1.cs
+++ pppl-uml-python/Form1.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace pppl_uml_python
 {
@@ -48,6 +47,120 @@ namespace pppl_uml_python
                 }
             }
 
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Parse JSON content using Newtonsoft.Json
+                JObject jsonObject = JObject.Parse(textBox1.Text);
+
+                // Generate Python code
+                string pythonCode = GeneratePythonCode(jsonObject);
+
+                // Display the generated Python code or save it to a file
+                textGeneratePython.Text = pythonCode;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating Python code: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string GeneratePythonCode(JObject jsonObject)
+        {
+            StringBuilder pythonCodeBuilder = new StringBuilder();
+
+            var modelArray = jsonObject["model"] as JArray;
+
+            if (modelArray != null)
+            {
+                foreach (var classDefinition in modelArray)
+                {
+                    string className = classDefinition["class_name"]?.ToString();
+                    string classAttributes = "";
+
+                    var attributesArray = classDefinition["attributes"] as JArray;
+
+                    if (attributesArray != null)
+                    {
+                        foreach (var attribute in attributesArray)
+                        {
+                            string attributeName = attribute["attribute_name"]?.ToString();
+                            string dataType = attribute["data_type"]?.ToString();
+
+                            if (!string.IsNullOrEmpty(attributeName) && !string.IsNullOrEmpty(dataType))
+                            {
+                                string pythonDataType = ConvertCSharpToPythonDataType(dataType);
+
+                                // Check for naming attribute or referential attribute
+                                if (attribute["attribute_type"]?.ToString() == "naming_attribute")
+                                {
+                                    classAttributes += $"    def __init__(self, {attributeName}):{Environment.NewLine}";
+                                }
+                                else if (attribute["attribute_type"]?.ToString() == "referential_attribute")
+                                {
+                                    // Referential attribute should not have default value
+                                    classAttributes += $"    def __init__(self, {attributeName}):{Environment.NewLine}";
+                                }
+                                else
+                                {
+                                    // Descriptive attribute with default value
+                                    string defaultValue = attribute["default_value"]?.ToString() ?? "";
+                                    classAttributes += $"    def __init__(self, {attributeName}={defaultValue}):{Environment.NewLine}";
+                                }
+                            }
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(className))
+                    {
+                        pythonCodeBuilder.AppendLine($"class {className}:{Environment.NewLine}{classAttributes}{Environment.NewLine}");
+                    }
+                }
+            }
+
+            return pythonCodeBuilder.ToString();
+        }
+
+
+
+        private string ConvertCSharpToPythonDataType(string csharpDataType)
+        {
+            switch (csharpDataType)
+            {
+                case "id":
+                    return "str";
+                case "integer":
+                    return "int";
+                case "real":
+                    return "float";
+                case "string":
+                    return "str";
+                case "state":
+                    return "str";
+                default:
+                    return csharpDataType;
+            }
+        }
+
+        private void bt_copyPy_Click(object sender, EventArgs e)
+        {
+            // Copy the generated Python code to the clipboard
+            Clipboard.SetText(textGeneratePython.Text);
+
+            // Optionally, provide user feedback
+            MessageBox.Show("Python code copied to clipboard!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void bt_copyJSON_Click(object sender, EventArgs e)
+        {
+            // Copy the generated Python code to the clipboard
+            Clipboard.SetText(textBox1.Text);
+
+            // Optionally, provide user feedback
+            MessageBox.Show("Python code copied to clipboard!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
