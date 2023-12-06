e9142e68c01400ccc90ff5d5d575d72a799e7918
eka <21106050023@student.uin-suka.ac.id>
2023-12-06 10:02:13 +0700

    upload file done

--- pppl-uml-python/Form1.Designer.cs
+++ pppl-uml-python/Form1.Designer.cs
@@ -31,9 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,40 +64,52 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "label2";
             // 
-            // button1
+            // btnGenerate
             // 
-            this.button1.Location = new System.Drawing.Point(57, 403);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Generate to Python";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGenerate.Location = new System.Drawing.Point(26, 403);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(202, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate to Python";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(416, 409);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(202, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear All Data";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(26, 22);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(202, 23);
+            this.btnUpload.TabIndex = 5;
+            this.btnUpload.Text = "Upload File";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(26, 51);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox1.Size = new System.Drawing.Size(370, 346);
             this.textBox1.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(275, 403);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(202, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Clear All Data";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -112,9 +125,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpload;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- pppl-uml-python/Form1.cs
+++ pppl-uml-python/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace pppl_uml_python
@@ -15,6 +16,38 @@ namespace pppl_uml_python
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string selectedFilePath;
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Set the filter for JSON files
+                openFileDialog.Filter = "JSON Files (*.json)|*.json|Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+
+                    try
+                    {
+                        // Read the content of the selected file
+                        string fileContent = File.ReadAllText(selectedFilePath);
+
+                        // Display the content in the TextBox
+                        textBox1.Text = fileContent;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle exceptions, e.g., file not found or unable to read
+                        MessageBox.Show($"Error reading the file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
         }
     }
 }
