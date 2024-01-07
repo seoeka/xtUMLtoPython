1803bdf90b77ca9e6188886e0c5096f0bbf2de85
eka <21106050023@student.uin-suka.ac.id>
2024-01-07 20:57:00 +0700

    design

--- pppl-uml-python/Form1.Designer.cs
+++ pppl-uml-python/Form1.Designer.cs
@@ -55,12 +55,12 @@ namespace pppl_uml_python
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label1.Location = new System.Drawing.Point(690, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 25);
+            this.label1.Size = new System.Drawing.Size(287, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "xtUML Model Compiler";
-            this.label1.Font = new System.Drawing.Font(this.label1.Font.FontFamily, 20);
             // 
             // label2
             // 
@@ -76,6 +76,8 @@ namespace pppl_uml_python
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.BackColor = System.Drawing.Color.DarkSalmon;
             this.label3.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
@@ -89,64 +91,68 @@ namespace pppl_uml_python
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.label4.Location = new System.Drawing.Point(710, 50);
-            this.label4.Name = "label1";
-            this.label4.Size = new System.Drawing.Size(130, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 18);
             this.label4.TabIndex = 0;
             this.label4.Text = "from xtUML JSON Model to Python";
-            this.label4.Font = new System.Drawing.Font(this.label1.Font.FontFamily, 11);
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.textGeneratePython);
             this.panel1.Location = new System.Drawing.Point(851, 120);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(639, 590);
             this.panel1.TabIndex = 1;
-            this.panel1.BackColor = Color.White;
             // 
             // textGeneratePython
             // 
             this.textGeneratePython.AutoSize = true;
+            this.textGeneratePython.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textGeneratePython.ForeColor = System.Drawing.Color.DarkSalmon;
             this.textGeneratePython.Location = new System.Drawing.Point(3, 9);
             this.textGeneratePython.Name = "textGeneratePython";
-            this.textGeneratePython.Size = new System.Drawing.Size(35, 13);
+            this.textGeneratePython.Size = new System.Drawing.Size(219, 17);
             this.textGeneratePython.TabIndex = 0;
-            this.textGeneratePython.ForeColor = System.Drawing.Color.DarkSalmon;
             this.textGeneratePython.Text = "translated python appears here...";
-            this.textGeneratePython.Font = new System.Drawing.Font(this.textGeneratePython.Font.FontFamily, 10);
             // 
             // btnGenerate
             // 
+            this.btnGenerate.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnGenerate.ForeColor = System.Drawing.Color.White;
             this.btnGenerate.Location = new System.Drawing.Point(30, 150);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(125, 40);
             this.btnGenerate.TabIndex = 4;
             this.btnGenerate.Text = "Translate";
-            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.UseVisualStyleBackColor = false;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
-            this.btnGenerate.Font = new System.Drawing.Font(this.btnGenerate.Font.FontFamily, 9);
-            this.btnGenerate.ForeColor = System.Drawing.Color.White;
-            this.btnGenerate.BackColor = System.Drawing.Color.DarkSalmon;
             // 
             // btnClear
             // 
+            this.btnClear.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
             this.btnClear.Location = new System.Drawing.Point(30, 610);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(125, 40);
             this.btnClear.TabIndex = 6;
             this.btnClear.Text = "Reset";
-            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.UseVisualStyleBackColor = false;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            this.btnClear.Font = new System.Drawing.Font(this.btnClear.Font.FontFamily, 9);
-            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
-            this.btnClear.BackColor = System.Drawing.Color.DarkSalmon;
             // 
             // btnUpload
             // 
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnUpload.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(139)))), ((int)(((byte)(139)))));
             this.btnUpload.Location = new System.Drawing.Point(30, 90);
             this.btnUpload.Name = "btnUpload";
             this.btnUpload.Size = new System.Drawing.Size(125, 40);
@@ -154,45 +160,44 @@ namespace pppl_uml_python
             this.btnUpload.Text = "Select File";
             this.btnUpload.UseVisualStyleBackColor = true;
             this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
-            this.btnUpload.Font = new System.Drawing.Font(this.btnUpload.Font.FontFamily, 9);
-            this.btnUpload.ForeColor = Color.FromArgb(0x00, 0x8B, 0x8B);
             // 
             // btnParse
             // 
+            this.btnParse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnParse.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(110)))), ((int)(((byte)(78)))));
             this.btnParse.Location = new System.Drawing.Point(30, 210);
             this.btnParse.Name = "btnParse";
             this.btnParse.Size = new System.Drawing.Size(125, 40);
             this.btnParse.TabIndex = 1;
             this.btnParse.Text = "Parse";
             this.btnParse.UseVisualStyleBackColor = true;
-            this.btnParse.Font = new System.Drawing.Font(this.btnParse.Font.FontFamily, 9);
-            this.btnParse.ForeColor = Color.FromArgb(0xE9, 0x6E, 0x4E);
             // 
             // btnVisualize
             // 
+            this.btnVisualize.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnVisualize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnVisualize.ForeColor = System.Drawing.Color.White;
             this.btnVisualize.Location = new System.Drawing.Point(30, 270);
             this.btnVisualize.Name = "btnVisualize";
             this.btnVisualize.Size = new System.Drawing.Size(125, 40);
             this.btnVisualize.TabIndex = 1;
             this.btnVisualize.Text = "Visualize";
-            this.btnVisualize.UseVisualStyleBackColor = true;
-            this.btnVisualize.Font = new System.Drawing.Font(this.btnVisualize.Font.FontFamily, 9);
-            this.btnVisualize.ForeColor = System.Drawing.Color.White;
-            this.btnVisualize.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnVisualize.UseVisualStyleBackColor = false;
             // 
             // btnSimulate
             // 
+            this.btnSimulate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnSimulate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(139)))), ((int)(((byte)(139)))));
             this.btnSimulate.Location = new System.Drawing.Point(30, 330);
             this.btnSimulate.Name = "btnSimulate";
             this.btnSimulate.Size = new System.Drawing.Size(125, 40);
             this.btnSimulate.TabIndex = 1;
             this.btnSimulate.Text = "Simulate";
             this.btnSimulate.UseVisualStyleBackColor = true;
-            this.btnSimulate.Font = new System.Drawing.Font(this.btnSimulate.Font.FontFamily, 9);
-            this.btnSimulate.ForeColor = Color.FromArgb(0x00, 0x8B, 0x8B);
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.textBox1.Location = new System.Drawing.Point(180, 120);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
@@ -200,11 +205,12 @@ namespace pppl_uml_python
             this.textBox1.Size = new System.Drawing.Size(640, 590);
             this.textBox1.TabIndex = 2;
             this.textBox1.WordWrap = false;
-            this.textBox1.Font = new System.Drawing.Font(this.textBox1.Font.FontFamily, 10);
             // 
             // bt_copyPy
-            //
+            // 
             this.bt_copyPy.Enabled = false;
+            this.bt_copyPy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bt_copyPy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(110)))), ((int)(((byte)(78)))));
             this.bt_copyPy.Location = new System.Drawing.Point(850, 725);
             this.bt_copyPy.Name = "bt_copyPy";
             this.bt_copyPy.Size = new System.Drawing.Size(150, 40);
@@ -212,12 +218,12 @@ namespace pppl_uml_python
             this.bt_copyPy.Text = "Copy Python";
             this.bt_copyPy.UseVisualStyleBackColor = true;
             this.bt_copyPy.Click += new System.EventHandler(this.bt_copyPy_Click);
-            this.bt_copyPy.Font = new System.Drawing.Font(this.bt_copyPy.Font.FontFamily, 10);
-            this.bt_copyPy.ForeColor = Color.FromArgb(0xE9, 0x6E, 0x4E);
             // 
             // bt_copyJSON
-            //
+            // 
             this.bt_copyJSON.Enabled = false;
+            this.bt_copyJSON.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bt_copyJSON.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(139)))), ((int)(((byte)(139)))));
             this.bt_copyJSON.Location = new System.Drawing.Point(180, 725);
             this.bt_copyJSON.Name = "bt_copyJSON";
             this.bt_copyJSON.Size = new System.Drawing.Size(150, 40);
@@ -225,12 +231,12 @@ namespace pppl_uml_python
             this.bt_copyJSON.Text = "Copy JSON";
             this.bt_copyJSON.UseVisualStyleBackColor = true;
             this.bt_copyJSON.Click += new System.EventHandler(this.bt_copyJSON_Click);
-            this.bt_copyJSON.Font = new System.Drawing.Font(this.bt_copyJSON.Font.FontFamily, 10);
-            this.bt_copyJSON.ForeColor = Color.FromArgb(0x00, 0x8B, 0x8B);
             // 
             // btExportPython
-            //
+            // 
             this.btExportPython.Enabled = false;
+            this.btExportPython.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btExportPython.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(110)))), ((int)(((byte)(78)))));
             this.btExportPython.Location = new System.Drawing.Point(30, 670);
             this.btExportPython.Name = "btExportPython";
             this.btExportPython.Size = new System.Drawing.Size(125, 40);
@@ -238,8 +244,6 @@ namespace pppl_uml_python
             this.btExportPython.Text = "Save";
             this.btExportPython.UseVisualStyleBackColor = true;
             this.btExportPython.Click += new System.EventHandler(this.btExportPython_Click);
-            this.btExportPython.Font = new System.Drawing.Font(this.btExportPython.Font.FontFamily, 9);
-            this.btExportPython.ForeColor = Color.FromArgb(0xE9, 0x6E, 0x4E);
             // 
             // btHelp
             // 
@@ -262,7 +266,7 @@ namespace pppl_uml_python
             // howToMenuItem
             // 
             this.howToMenuItem.Name = "howToMenuItem";
-            this.howToMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.howToMenuItem.Size = new System.Drawing.Size(166, 22);
             this.howToMenuItem.Text = "How to Use";
             this.howToMenuItem.Click += new System.EventHandler(this.howToMenuItem_Click);
             // 
@@ -277,7 +281,7 @@ namespace pppl_uml_python
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 475);
+            this.ClientSize = new System.Drawing.Size(1184, 681);
             this.Controls.Add(this.btHelp);
             this.Controls.Add(this.btExportPython);
             this.Controls.Add(this.bt_copyJSON);
@@ -297,11 +301,13 @@ namespace pppl_uml_python
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UML to Python";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
         #endregion
         private System.Windows.Forms.Label label1;
